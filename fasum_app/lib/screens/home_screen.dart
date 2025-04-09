c4c08770b3cbb87c53117ceccae9a0ae3445e3d4
V10cio <161033040+V10cio@users.noreply.github.com>
2025-04-09 19:51:17 +0700

    fasum-app

--- fasum_app/lib/screens/home_screen.dart
+++ fasum_app/lib/screens/home_screen.dart
@@ -2,37 +2,42 @@ import 'package:fasum_app/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
-  Future<void> signOut(BuildContext context) async {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
 
-    if (context.mounted) {
+    if (mounted) { // Pastikan widget masih ada di tree
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => SignInScreen()),
       );
     }
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text("Home"),
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      actions: [
-        IconButton(
-          onPressed: () {
-            signOut(context);
-          },
-          icon: const Icon(Icons.logout),
-        ),
-      ],
-    ), 
-    body: const Center(
-      child: Text("You have logged in"),
-    ), 
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ), 
+      body: const Center(
+        child: Text("You have logged in"),
+      ), 
+    );
+  }
 }
